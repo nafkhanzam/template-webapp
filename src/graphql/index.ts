5207a0f46537e6e3a9b314be4b92b6ea3fddbc58
nafkhanzam <nafkhanalzamzami@gmail.com>
2021-05-09 11:57:48 +0700

    handle no resource.ts

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,9 +1,16 @@
 import {metas} from "@/constants";
 import {GraphQLClient} from "graphql-request";
-import {getSdk} from "./resource";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let getSdkFunc: any;
+try {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const {getSdk} = require("./resource");
+  getSdkFunc = getSdk;
+} catch (error) {}
 
 export const createResourceGqlClient = (headers?: Record<string, string>) =>
-  getSdk(new GraphQLClient(metas.graphqlApiUrl, {headers}));
+  getSdkFunc?.(new GraphQLClient(metas.graphqlApiUrl, {headers})) ?? {};
 export const createAuthedResourceGqlClient = (token: string) =>
   createResourceGqlClient({Authorization: `Bearer ${token}`});
 
